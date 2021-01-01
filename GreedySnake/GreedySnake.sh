8480da56ca0e3cfdc64ebed1c340d28ddd6c090f
root <root@Elven.localdomain>
2021-01-01 00:42:17 -0700

    GreedySnakeV1.1

--- GreedySnake/GreedySnake.sh
+++ GreedySnake/GreedySnake.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-cd "/root/Cpp/GreedySnake/" && g++ main.cpp snakebody.cpp map.cpp -g -pthread -o main && ./main && rm main
+work_path=$(dirname $0)
+cd ${work_path}
+work_path=$(pwd)
+cd ${work_path}
+g++ main.cpp snakebody.cpp map.cpp -g -pthread -o main && ./main && rm main
